d567bd31096250d96cb723befc23c53b24aabdb5
kaaarolin <karolin_57@hotmail.com>
2025-10-30 16:30:42 +0100

    second commit

---
+++ Frontend/src/pages/Favorites.jsx
@@ -0,0 +1,8 @@
+function Favorite() {
+    return <div className="favorites-empty">
+        <h2>No favorite anime yet</h2>
+        <p>Start adding anime to your favorites</p>
+    </div>
+}
+
+export default Favorite
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,4 +1,9 @@
+import MovieCard from "../components/MovieCard"
+import { useState } from "react"
+
 function Home() {
+
+    const [searchQuery, setSearchQuery] = useState("");
 
     const movies = [
         {id: 1, title: "John Wick", release_date: "2023"}, 
@@ -6,7 +11,28 @@ function Home() {
         {id: 3, title: "The Lion king", release_date: "2023"}
     ]
 
+    const handleSearch = (e) => {
+        e.preventDefault()
+        alert(searchQuery)
+        setSearchQuery("")
+    };
+
     return <div className="home">
-        <div className="movies-grid"></div>
+
+        <form onSubmit={handleSearch} className="search-form">
+            <input type="text" placeholder="Search for movies . . . " className="search-input" 
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <button type="submit" className="search-button">Search</button>
+        </form>
+
+        <div className="movies-grid">
+            {movies.map(movie => ( 
+                movie.title.toLowerCase().startsWith(searchQuery) && <MovieCard movie={movie} key={movie.id} />
+            ))}
+        </div>
     </div>
 }
+
+export default Home
